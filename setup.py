b6e4bb3a13e2a95744bdc31a1b9d6640f417b863
f3rn0s <tealkine2@gmail.com>
2018-05-20 20:14:01 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='socc',
       version='0.1',
       description='Quick and dirty socc generator with built-in decode/encode',
-      url='',
+      url='https://github.com/f3rn0s/socc',
       author='f3rn0s',
       author_email='',
       license='',
